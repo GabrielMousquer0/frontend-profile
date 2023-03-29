a9ce4e89939ff66e308d45c55f17f96832e88332
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-03-29 08:17:55 -0300

    🎨 Componentizando cabeçalho e inputs do login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
+import HeaderBar from './components/HeaderBar.vue';
+import InputLogin from './components/login/InputsLogin.vue';
+
 
 </script>
 
 <template>
-
+    <div>
+        <HeaderBar />
+        <span class="row justify-center fixed-center">
+            <InputLogin/>
+        </span>
+    </div>
 </template>
