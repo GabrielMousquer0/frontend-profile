22d5c278583fabc7353d5a908b0d10c9140c8acd
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-03-29 08:20:07 -0300

    💄 Adicionando o cabeçalho

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <q-layout view="lHh lpr lFf">
+            <q-header elevated>
+                <q-toolbar class="q-ma-md justify-center">
+                    <q-avatar size="100px" icon="person" />
+                    <span class="textStyle text-h1">Login</span>
+                </q-toolbar>
+            </q-header>
+        </q-layout> 
+    </div>
+</template>
+
+<style>
+.textStyle {
+    font-size: 5rem;
+    font-family: "Open Sans", sans-serif;
+}
+</style>
