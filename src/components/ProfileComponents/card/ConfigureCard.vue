ebb1f19beecd0fbb01bfdd3116c391d45ffd6467
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-04 11:35:21 -0300

    ✨ Implementando o edit user, podendo editar avatar por link, editar nome de usuario, e-mail e senha

--- src/components/ProfileComponents/card/ConfigureCard.vue
+++ src/components/ProfileComponents/card/ConfigureCard.vue
@@ -1,50 +1,57 @@
 <script setup>
 import { useUserStore } from '../../../store/UserStore';
-import { edit } from '../../../schemas/'
+import { EmailUpdate, PasswordUpdate, UsernameUpdate } from '../../../schemas/'
 import { ref, useQuasar, useMutation } from '../../../utils/'
 
 const store = useUserStore()
-const usernameEdit = ref(store.getUsername)
-const emailEdit = ref(store.getEmail)
-const passwordEdit = ref(store.getPassword)
+const usernameEdit = ref('')
+const emailEdit = ref('')
+const passwordEdit = ref('')
 const { notify } = useQuasar()
 
-const { execute } = useMutation(edit)
+const emailUp = useMutation(EmailUpdate)
+const usernameUp = useMutation(UsernameUpdate)
+const passwordUp = useMutation(PasswordUpdate)
 
-function editButton(username, email, password, id) {
-    if(username === store.getUsername|| !username && email === store.getEmail || !email && password === store.getPassword || !password) return notify({message: 'Preencha o campo', icon: 'warning', color: 'negative'})
-if(username !== store.getUsername) {
-        execute({
+function editUsername(username, id) {
+    console.log(id)
+    if(!username) return notify({ message: 'Preencha o campo', icon: 'warning', color: 'warning'})
+    usernameUp.execute({
             username,
             id
         }).then(({data}) => {
-            return notify({message: `User alterado para: ${username}`, icon: 'check', color: 'positive'})
+            store.user_username = username
+            return notify({message: `User alterado para: ${username}, atualize a pagina!`, icon: 'check', color: 'positive'})
         })
+
 }
-if(email !== store.getEmail) {
-        execute({
+function editEmail(email, id) {
+    if(!email) return notify({ message: 'Preencha o campo', icon: 'warning', color: 'warning' })
+    console.log(emailUp.execute())
+        emailUp.execute({
             email,
             id
         }).then(({data}) => {
             return notify({message: `E-mail alterado para: ${email}`, icon: 'check', color: 'positive'})
         })
-} 
-if(password !== store.getPassword) {
-        execute({
+
+}
+function editPassword(password, id) {
+    if(!password) return notify({ message: 'Preencha o campo', icon: 'warning', color: 'warning'})
+        passwordUp.execute({
             password,
             id
         }).then(({data}) => {
             return notify({message: `Senha alterado para: ${password}`, icon: 'check', color: 'positive'})
         })
 }
-}
 </script>
 
 <template>
     <span class="my-configs h1-text">Configs</span>
-        <q-input v-model="usernameEdit" rounded outlined class="configUsername" :label="'Username: ' + store.getUsername"/> <q-btn @click="editButton(usernameEdit, emailEdit, passwordEdit, store.getId)" class="btnUsername" round icon="edit" color="transparent"/>
-        <q-input v-model="emailEdit" rounded outlined class="configEmail" :label="'E-mail: ' + store.getEmail"/> <q-btn @click="editButton(usernameEdit, emailEdit, passwordEdit, store.getId)" class="btnEmail" round icon="edit" color="transparent"/>
-        <q-input v-model="passwordEdit" rounded outlined class="configPassword" :label="'Senha: ' + store.getPassword"/> <q-btn @click="editButton(usernameEdit, emailEdit, passwordEdit, store.getId)" class="btnPassword" round icon="edit" color="transparent"/>
+        <q-input v-model="usernameEdit" rounded outlined class="configUsername" label="Username"/> <q-btn @click="editUsername(usernameEdit, store.getId)" class="btnUsername" round icon="edit" color="transparent"/>
+        <q-input v-model="emailEdit" rounded outlined class="configEmail" label="E-mail"/> <q-btn @click="editEmail(emailEdit, store.getId)" class="btnEmail" round icon="edit" color="transparent"/>
+        <q-input v-model="passwordEdit" rounded outlined class="configPassword" label="Senha"/> <q-btn @click="editPassword(passwordEdit, store.getId)" class="btnPassword" round icon="edit" color="transparent"/>
 </template>
 
 <style scoped>
--- src/components/ProfileComponents/card/InformationCard.vue
+++ src/components/ProfileComponents/card/InformationCard.vue
@@ -1,27 +1,60 @@
 <script setup>
-import { useUserStore } from '../../../store/UserStore.js'
+import { useUserStore } from '../../../store/UserStore.js';
+import { ref, useMutation, useQuasar } from '../../../utils/';
+import { AvatarUpdate } from '../../../schemas';
 
 const store = useUserStore()
+const username = store.getUsername.length < 10 ? store.getUsername : store.getUsername.slice(0, 10) + '...';
+const avatarURL = ref('')
+const { notify } = useQuasar()
+
+const { execute } = useMutation(AvatarUpdate)
+
+function iconSend(avatar, id) {
+    execute({
+        avatar,
+        id
+    }).then(({data}) => {
+
+        store.user_avatar = data.editAvatar.avatar
+        return notify({message: 'Foto de perfil alterada com sucesso', icon: 'check', color: 'positive'})
+    }).catch((e) => {
+        return notify({message: 'error: Me dê uma url menor!', icon: 'warning', color: 'orange'})
+    })
+    
+}
 </script>
 
 <template>
-    <q-avatar rounded class="myIcon" size="200px">  <img :src="store.getAvatar"> </q-avatar> 
-    <q-btn class="btnPassword" round icon="edit" color="primary"/>
-    <span class="myUsername h1-text">{{ store.getUsername }}</span>
+    <q-avatar rounded class="myIcon" size="200px"> <img :src="store.getAvatar"> </q-avatar>
+    <q-btn class="btnPassword" round icon="edit" color="primary">
+        <q-popup-edit v-model="avatarURL" v-slot="scope">
+            <q-input v-model="avatarURL" hint="Me dê uma URL de alguma imagem" rounded dense @keyup.enter="scope.set">
+                <q-btn class="iconSend" flat @click="iconSend(avatarURL, store.getId)" icon="add" /> </q-input>
+        </q-popup-edit>
+    </q-btn>
+    <span class="myUsername h1-text">{{ username }}</span>
 </template>
 
 <style scoped>
+.iconSend {
+    position: absolute;
+    left: 155px;
+}
+
 .myUsername {
     position: absolute;
     top: 400px;
     left: 130px;
     font-size: 60px;
 }
+
 .myIcon {
     position: absolute;
     top: 200px;
     left: 25%;
 }
+
 .btnPassword {
     position: absolute;
     top: 350px;
