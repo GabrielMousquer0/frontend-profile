7bd7e3ef76b8a4324f5a22ff19646bbc0c1faf5e
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-05 14:08:33 -0300

    ✨ IMportando moment e ms no utils para exportação rapida

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,5 +3,16 @@ import { useMutation } from 'villus';
 import { useQuasar } from 'quasar';
 import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
+import moment from 'moment';
+import ms from 'ms';
 
-export { ref, computed, useMutation, useQuasar, useRouter, storeToRefs };
+export {
+  ref,
+  computed,
+  useMutation,
+  useQuasar,
+  useRouter,
+  storeToRefs,
+  moment,
+  ms,
+};
