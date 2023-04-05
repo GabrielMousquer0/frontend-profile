5e3578b2aa95e10d4e2048acb4c9d58a051af007
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-05 14:09:08 -0300

    🎨 Componentização da outra area do perfil do usuario

--- src/views/user/ProfileUser.vue
+++ src/views/user/ProfileUser.vue
@@ -1,27 +1,28 @@
 <script setup>
 import ProfileCard from '../../components/ProfileComponents/card/InformationCard.vue'
 import InformationCard from '../../components/ProfileComponents/card/ConfigureCard.vue'
+import UserInformation from '../../components/ProfileComponents/user/UserInformations.vue'
 </script>
 
 <template>
-
-<q-card class="myCard">
-    <q-card-section>
-        <ProfileCard />
-    </q-card-section>
-    <q-card-section>
-        <InformationCard />
-    </q-card-section>
-</q-card>
+    <q-card class="myCard">
+        <q-card-section>
+            <ProfileCard />
+        </q-card-section>
+        <q-card-section>
+            <InformationCard />
+        </q-card-section>
+    </q-card>
+        <UserInformation/>
 
 </template>
 
 <style scoped>
 .myCard {
-width: 500px;
-height: 1000px;
-right: 137%;
-background-color: rgb(125, 126, 128);
+    width: 500px;
+    height: 1000px;
+    right: 137%;
+    background-color: rgb(125, 126, 128);
 }
 
 </style>
