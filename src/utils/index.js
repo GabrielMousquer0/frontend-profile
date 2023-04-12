a755bfcd1502a2ffc04815046008f188bf89424e
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-12 08:49:14 -0300

    ✨ Adicionando o use query ao utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 import { ref, computed } from 'vue';
-import { useMutation } from 'villus';
+import { useMutation, useQuery } from 'villus';
 import { useQuasar } from 'quasar';
 import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
@@ -10,6 +10,7 @@ export {
   ref,
   computed,
   useMutation,
+  useQuery,
   useQuasar,
   useRouter,
   storeToRefs,
