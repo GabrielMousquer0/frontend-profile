0d490af31687c0559e2ce492e19655f66848e6a8
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-04-13 08:05:42 -0300

    ✨ Criando botao para logout e mostrar os usuarios

--- src/components/ProfileComponents/user/UserInformations.vue
+++ src/components/ProfileComponents/user/UserInformations.vue
@@ -2,13 +2,26 @@
 import TecnologyUser from './profile/TecnologyUser.vue';
 import InfoUser from './profile/InfoUser.vue';
 import DescriptionUser from './profile/DescriptionUser.vue';
+import ButtonMenu from './profile/ButtonMenu.vue';
 import { routerStore } from '../../../store/RouterStore'
+import { useUserStore } from '../../../store/UserStore';
+import { useRouter } from '../../../utils';
 
+const userStore = useUserStore()
 const store = routerStore() 
+const router = useRouter()
 store.router_name = 'User'
+
+
+if(!userStore.getId) {
+			router.push({
+				path: '/'
+			})
+		}
 </script>
 
 <template>
+    <ButtonMenu/>
     <InfoUser/>
     <DescriptionUser/>
     <TecnologyUser/>
