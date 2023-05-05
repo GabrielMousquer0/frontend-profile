217ed2afa8fa178b20f036a018ab1e0ef7f780b6
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-05-05 10:30:19 -0300

    ✨ Criando nova function para receber notificações negativas e positivas

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,9 @@
 
-import { runMutation, runQuery } from './functions';
+import { runMutation, runQuery, negativeNotify, positiveNotify } from './functions';
 
 export {
   runMutation,
   runQuery,
+  negativeNotify,
+  positiveNotify,
 };
