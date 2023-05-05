e8cf63262cb4095371acb462f2a7a8b5f01078c6
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-05-05 10:07:05 -0300

    🔥 Importações de forma incorreta

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,23 +1,7 @@
-import { ref, reactive, computed, onMounted } from 'vue';
-import { useQuasar } from 'quasar';
-import { useRouter } from 'vue-router';
-import { storeToRefs } from 'pinia';
-import moment from 'moment';
-import ms from 'ms';
+
 import { runMutation, runQuery } from './functions';
-import { routes } from './routes';
 
 export {
   runMutation,
   runQuery,
-  ref,
-  reactive,
-  computed,
-  useQuasar,
-  useRouter,
-  storeToRefs,
-  moment,
-  ms,
-  routes,
-  onMounted,
 };
