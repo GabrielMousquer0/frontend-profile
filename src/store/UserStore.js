732e413989b078558a1e1e2f9ee9b1d615b8636f
gabrielmousquer <gabrielmousquer1227@gmail.com>
2023-05-09 16:40:59 -0300

    ✨ Dark mode

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -3,11 +3,20 @@ import { defineStore } from 'pinia';
 export const useUserStore = defineStore('useUserStore', {
   state: () => ({
     user: {},
+    mode: false,
   }),
   getters: {
     getUser: (state) => {
       return state.user;
+    },
+    getMode: (state) => {
+      return state.mode;
     }
+  },
+  actions: {
+    tradeMode(value) {
+      return this.$state.mode = value;
+    },
   },
   persist: true,
 });
